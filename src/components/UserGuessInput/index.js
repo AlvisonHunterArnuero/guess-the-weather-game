181f63068fbf44c2a0306557bc07cdbcfbbbb1fd
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-10-08 11:27:07 -0600

    Added pattern for only numbers to the input field and avoid any other characters except the dot to divide decimals in floating numbers

--- src/components/UserGuessInput/index.js
+++ src/components/UserGuessInput/index.js
@@ -1,25 +1,40 @@
 import React from "react";
+
 const UserGuessInput = (props) => {
+  const strcustomTitle = "Type in only numbers, please."
   return (
     <div className='input-group mb-3'>
-    <input
-      value={props.temperature || ""}
-      onChange={props.handleInputChange}
-      onFocus={(e) => e.target.value = ""}
-      type='text'
-      placeholder={props.fillerText}
-      className='form-control'
-      aria-label={props.placeholder}
-      aria-describedby='button-addon2'
-    />
-    <button
-      className='btn btn-outline-secondary'
-      type='button'
-      id='button-addon2'
-      onClick={props.handleSubmit}
-    > Submit
-    </button>
-  </div>
+      <input
+        type='text'
+        data-bs-toggle='tooltip'
+        data-bs-placement='bottom'
+        data-bs-html='true'
+        placeholder={props.fillerText}
+        className='form-control'
+        aria-label={props.placeholder}
+        aria-describedby='button-addon2'
+        maxLength='5'
+        title={strcustomTitle}
+        pattern='[0-9.]{0,5}'
+        value={props.temperature || ""}
+        onChange={props.handleInputChange}
+        onFocus={(e) => (e.target.value = "")}
+        onKeyPress={(event) => {
+          if (!/[0-9.]/.test(event.key)) {
+            event.preventDefault();
+          }
+        }}
+      />
+      <button
+        className='btn btn-outline-secondary'
+        type='button'
+        id='button-addon2'
+        onClick={props.handleSubmit}
+      >
+        {" "}
+        Submit
+      </button>
+    </div>
   );
 };
 
